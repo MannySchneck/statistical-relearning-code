e37364c7a184be6be1c94066ceec427012e0fddc
Manny Schneck <manny.schneck@gmail.com>
2022-01-20 11:10:33 -0600

    finish setup

--- chapter_1/setup.r
+++ chapter_1/setup.r
@@ -1,7 +1,14 @@
+# PREREQS
+# pacman -S gcc-fortran extras/libgit2
+
 # Set up: install  RStan
 install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
 
 # install rethinking
+
+install.packages("remotes")
+remotes::install_github("stan-dev/cmdstanr")
+
 install.packages(c("coda","mvtnorm","devtools","dagitty"))
 library(devtools)
 devtools::install_github("rmcelreath/rethinking")
